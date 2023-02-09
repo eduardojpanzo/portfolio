4d5cbc8e57c7999641dd94c13780635d3db41bbe
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-02-09 09:17:49 +0100

    styles: pricipal Banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,3 +1,36 @@
+import Image from "next/image";
+
 export function Banner() {
-  return <div className="h-96">Banner</div>;
+  return (
+    <div className="h-96 flex items-center justify-around">
+      <div>
+        <span className="text-xl font-medium">Hello! I'm </span>
+
+        <h2 className="text-4xl text-sky-600">
+          João <br />
+          Eduardo Panzo
+        </h2>
+
+        <h3 className="leading-9">FullStack Devloper</h3>
+
+        <div className="flex gap-4 mt-4">
+          <p className="text-green-700 uppercase flex gap-2 items-center">
+            <em className="text-6xl">2</em> years of <br />
+            experience
+          </p>
+
+          <p className="text-green-700 uppercase flex gap-2 items-center">
+            <em className="text-6xl">4</em> Projects <br />
+            Completed
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <div className="w-52 h-52 rounded-full shadow-lg overflow-hidden">
+          <Image width={200} height={200} src={"/profil.png"} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
