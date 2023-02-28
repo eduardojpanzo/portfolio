2c330a72fb469e708bcb6dac1e87c0fdeca0912c
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-02-28 20:08:02 +0100

    improve the formation setions

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -2,11 +2,11 @@ import { SKillProps } from "../types/componetsTypes";
 
 export function Skill({ value, children, onClick }: SKillProps) {
   return (
-    <li className="h-32 overflow-hidden block bg-slate-100 rounded-xl dark:bg-slate-500">
+    <li className="h-32 overflow-hidden block bg-slate-200 rounded-xl transition-all border border-transparent hover:border-slate-500 dark:bg-slate-700 dark:hover:border-slate-100">
       <button
         onClick={onClick}
         type="button"
-        className="w-full h-full p-2 flex flex-col justify-between items-center text-sm font-normal "
+        className="w-full h-full p-2 flex flex-col justify-between items-center text-sm font-normal grayscale hover:grayscale-0"
       >
         {children}
         {value}
--- src/sections/Formations.tsx
+++ src/sections/Formations.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { SectionContsiner } from "../components/SectionContsiner";
 import { Title } from "../components/Title";
 
@@ -6,11 +7,22 @@ export function Formations() {
     <SectionContsiner>
       <Title name="Formation" />
 
-      <div>
-        Dline-code <span>Full Stack JavaScript - Course</span>
-      </div>
-      <div>
-        3º Ano - INSTIC <span>Computers Engineering</span>
+      <div className="flex items-center justify-center flex-wrap">
+        <div className="w-80 h-80 bg-slate-800 flex flex-col justify-between items-center rounded-3xl p-4 mx-4 my-8 dark:bg-slate-200">
+          <Image src={""} alt="" />
+          <div className="text-slate-50 dark:text-slate-800 text-center">
+            <p className="text-xl">Engenharia Informática</p>
+            <p>2020 - Em curso</p>
+          </div>
+        </div>
+
+        <div className="w-80 h-80 bg-slate-800 flex flex-col justify-between items-center rounded-3xl p-4 mx-4 my-8 dark:bg-slate-200">
+          <Image src={""} alt="" />
+          <div className="text-slate-50 dark:text-slate-800 text-center">
+            <p className="text-xl">Full-Stack Developer</p>
+            <p>2021 - 2022</p>
+          </div>
+        </div>
       </div>
     </SectionContsiner>
   );
