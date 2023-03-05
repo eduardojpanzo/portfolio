7f2ba9b194d62a5a69a2ec14305e77403607368a
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-03-05 19:10:12 +0100

    feat: improve the components

--- src/components/Networking.tsx
+++ src/components/Networking.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
 import { networkItems } from "../mock";
 
-export function Networking() {
+interface NetworkingProps {
+  float?: boolean;
+}
+
+export function Networking({ float }: NetworkingProps) {
   return (
-    <div className="flex gap-4 pb-3">
+    <div
+      className={`flex gap-4 pb-3 ${
+        float ? `flex-col absolute left-0 bottom-0` : ``
+      }`}
+    >
       {networkItems.map(({ href, Icon }) => (
         <Link target="_blank" href={href}>
           <Icon />
