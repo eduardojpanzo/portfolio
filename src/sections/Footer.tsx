3aca3202c4ee7d652083881cd71061e102be9a30
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2024-01-01 10:31:10 +0100

    fix:correção do menu footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,12 +1,21 @@
+import { useTranslations } from "next-intl";
 import { Menu } from "../components/Menu";
 import { Networking } from "../components/Networking";
 
 export function Footer() {
+  const t = useTranslations("Menu");
+  const menuLinks = [
+    { path: "#about", value: `${t("about")}` },
+    { path: "#skill", value: `${t("skill")}` },
+    { path: "#formation", value: `${t("formation")}` },
+    { path: "#projects", value: `${t("projects")}` },
+    { path: "#contacts", value: `${t("contacts")}` },
+  ];
   return (
     <footer>
       <div className="max-w-7xl my-0 mx-auto px-5 pt-7 flex flex-col items-center gap-2 py-2">
         <p className="text-2xl ">João Eduardo Panzo</p>
-        <Menu className="pb-3" />
+        <Menu menuLinks={menuLinks} />
         <Networking />
         <p className="text-xs">
           Copyright © | Coded with 💚 by João Eduardo Panzo
